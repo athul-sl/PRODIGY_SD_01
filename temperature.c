c41c7f845de26cb331ac33034c14e7e591388de5
athul-sl <athulsl2505@gmail.com>
2024-08-13 18:05:07 +0530

    temperature.c

--- temperature.c
+++ temperature.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
-float c_to_f(int temp)
+float c_to_f(float temp)
 {
   return (temp * (9/5.0))+32;
 }
-float c_to_k(int temp)
+float c_to_k(float temp)
 {
   return temp+273.15;
 }
-float f_to_c(int temp)
+float f_to_c(float temp)
 {
   return (temp-32)*(5/9.0);
 }
-float f_to_k(int temp)
+float f_to_k(float temp)
 {
   return f_to_c(temp)+273.15;
 }
-float k_to_c(int temp)
+float k_to_c(float temp)
 {
   return temp-273.15;
 }
-float k_to_f(int temp)
+float k_to_f(float temp)
 {
   return c_to_f(k_to_c(temp));
 }
 
 void main()
 {
-  int temp;
+  float temp;
   char t;
-  printf("Enter unit of temperature to be converted : ");
+  printf("Enter unit of temperature to be converted (C : Celcius; F : Farenheit; K : Kelvin ) : ");
   scanf("%c",&t);
-  printf("\nEnter value of temperature (C : Celcius; F : Farenheit; K : Kelvin ): ");
+  printf("\nEnter value of temperature : ");
   scanf("%f",&temp);
-  if(t=="c" ||t=="C")
+  if(t=='c' ||t=='C')
     {
-      printf("Farenheit : %f°F\nKelvin : %fK",c_to_f(temp),c_to_k(temp));
+      printf("Celcius : %.3f°C\nFarenheit : %.3f°F\nKelvin : %.3fK",temp,c_to_f(temp),c_to_k(temp));
     }
-  else if(t=="f" ||t=="F")
+  else if(t=='f' ||t=='F')
     {
-      printf("Celcius : %f°C\nKelvin : %fK",f_to_c(temp),f_to_k(temp));
+      printf("Farenheit : %.3f°F\nCelcius : %.3f°C\nKelvin : %.3fK",temp,f_to_c(temp),f_to_k(temp));
     }
-  else if(t=="k" ||t=="K")
+  else if(t=='k' ||t=='K')
     {
-      printf("Celcius : %f°C\nFarenheit : %f°F",k_to_c(temp),k_to_f(temp));
+      printf("Kelvin : %.3fK\nCelcius : %.3f°C\nFarenheit : %.3f°F",temp,k_to_c(temp),k_to_f(temp));
     }
 }
